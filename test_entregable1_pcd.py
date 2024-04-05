fbba9e0fcada04f249d81796cbb0f9c396fe0111
rubengilmartinez <ruben.gilm.upct@gmail.com>
2024-04-05 19:39:11 +0200

    Añadimos todo el código de prueba para probar los métodos, nuevos métodos definidos y corrección de algunos errores menores en los métodos de instancia.

--- test_entregable1_pcd.py
+++ test_entregable1_pcd.py
@@ -57,7 +57,7 @@ class Estudiante(Persona):
         self.asignaturas_completadas.append((asignatura.nombre, calificacion))
     
     def añadir_asignatura_matriculada(self, asignatura):
-        self. asignaturas_cursando.append(asignatura)
+        self.asignaturas_cursando.append(asignatura)
     
     def eliminar_asignatura_matriculada(self, asignatura):
         self.asignaturas_cursando.remove(asignatura)
@@ -238,15 +238,16 @@ class Universidad:
         miembro = self.__buscar_profesor(dni)
         if miembro == "DNI no coincide":
             miembro = self.__buscar_investigador(dni)
-            return f"{miembro.nombre} se ha cambiado al departamento {miembro.departamento}"
+            
         
         if miembro != "DNI no coincide":
-            self.eliminar_miembro_dep(miembro.dni)  # Pasamos el DNI del miembro
-            miembro.cambio_dep(nuevo_departamento)
+            self.eliminar_miembro_dep(miembro.dni)  # Eliminamos al miembro de su antiguo dep
+            miembro.cambio_dep(nuevo_departamento) # Actualizamos info nuevo dep
             self.añadir_miembro_dep(miembro.dni)  # Añadimos el miembro al nuevo departamento
             return f"{miembro.nombre} se ha cambiado al departamento {miembro.departamento}"
         else:
             return "DNI no coincide"
+
 
     def eliminar_estudiante(self, dni):
         estudiante = self.__buscar_estudiante(dni)
@@ -265,8 +266,8 @@ class Universidad:
             for asignatura in prof.asignaturas_a_impartir:
                 asignatura.eliminar_profesor(prof)
 
-        if inves is not None and inves != prof:  # Asegurarse de que el investigador no es el mismo que el profesor
-            try:
+        if inves is not None and inves != prof:  # Asegurarse de que no se utiliza el mismo objeto, nos permitirá 
+            try:                                  # tratar cuando tratamos de eliminar un P.titular, también lo borramos como investigador.
                 self.investigadores.remove(inves)
             except ValueError:
                 print(f"DNI no coincide")
@@ -279,13 +280,13 @@ class Universidad:
             self.eliminar_miembro_dep(dni)
     
     def añadir_asignatura_a_cursar(self, dni, id_asig):
-        if len(self.asignaturas_cursando) > 12:
-            raise ValueError('No se pueden cursar más de 12 asignaturas al mismo tiempo.')
         asignatura = self.__buscar_asignatura(id_asig)
         estudiante = self.__buscar_estudiante(dni)
+        if len(estudiante.asignaturas_cursando) > 12:
+            raise ValueError('No se pueden cursar más de 12 asignaturas al mismo tiempo.')
         estudiante.añadir_asignatura_matriculada(asignatura)
         asignatura.añadir_estudiantes(estudiante)
-        return f"{estudiante.nombre} se ha matriculado en la asignatura de {asignatura.nombre}"
+        print(f"{estudiante.nombre} se ha matriculado en la asignatura de {asignatura.nombre}\n")
 
     def aprobar_asignatura(self, dni, id_asig, calificacion):
         if calificacion < 5:
@@ -296,39 +297,55 @@ class Universidad:
         estudiante.creditos_completados += asignatura.creditos
         estudiante.eliminar_asignatura_matriculada(asignatura)
         asignatura.eliminar_estudiantes(estudiante)
-        return f"{estudiante.nombre} ha aprobado la asignatura {asignatura.nombre} con una calificación de {calificacion}"
+        print(f"{estudiante.nombre} ha aprobado la asignatura {asignatura.nombre} con una calificación de {calificacion}\n")
 
-
+    
     def visualizar_boletin_de_calificaciones(self, dni):
         estudiante = self.__buscar_estudiante(dni)
-        for calificacion in estudiante.asignaturas_completadas:
-            print(calificacion)
-    
-    """def visualizar_boletin_de_calificaciones(self, dni):
-        estudiante = self.__buscar_estudiante(dni)
+        print(f"Boletín de Calificaciones de {estudiante.nombre}:\n")
         for asig, nota in estudiante.asignaturas_completadas:
-            print(f"Calificaciones:\n")
+            
             print(f"{asig}: {nota}\n")
-            print(f"Nota media: {sum(nota)//len(estudiante.asignaturas_completadas)}")
-    """
+            
+    
 
     def visualizar_creditos_completados(self, dni):
         estudiante = self.__buscar_estudiante(dni)
-        return f"{estudiante.nombre} lleva {estudiante.creditos_completados} créditos completados."
+        print(f"{estudiante.nombre} lleva {estudiante.creditos_completados} créditos completados.\n")
+
+
+    def visualizar_est_matriculados_y_profesores_asig(self, id_asig):
+        asignatura = self.__buscar_asignatura(id_asig)
+        print(f"Profesores y estudiantes de {asignatura.nombre}: \n Profesores:")
+        for p in asignatura.profesores:
+            print(f"{p.nombre}\n")
+        print("Alumnos matriculados:")
+        for e in asignatura.estudiantes_matriculados:
+            print(f"{e.nombre}\n")
+
 
     def asignar_profesorado_a_asignatura(self, dni, id_asig):
         prof = self.__buscar_profesor(dni)
         asignatura = self.__buscar_asignatura(id_asig)
         prof.añadir_asignatura_a_impartir(asignatura)
         asignatura.añadir_profesor(prof)
-        return f"Al profesor {prof.nombre} se le ha asignado la asignatura de {asignatura.nombre} para el grado de {asignatura.grado}"
+        print(f"Al profesor {prof.nombre} se le ha asignado la asignatura de {asignatura.nombre} para el grado de {asignatura.grado}\n")
     
+
     def eliminar_profesorado_de_asignatura(self, dni, id_asig):
         prof = self.__buscar_profesor(dni)
         asignatura = self.__buscar_asignatura(id_asig)
         prof.eliminar_asignatura_a_impartir(asignatura)
         asignatura.eliminar_profesor(prof)
-        return f"El profesor {prof.nombre} dejará de impartir la asignatura de {asignatura.nombre} en el grado de {asignatura.grado}"
+        print(f"El profesor {prof.nombre} dejará de impartir la asignatura de {asignatura.nombre} en el grado de {asignatura.grado}\n")
+
+
+    def visualizar_asignaturas_de_profesor(self, dni):
+        prof = self.__buscar_profesor(dni)
+        print(f"El profesor/a {prof.nombre} está impartiendo clase en: \n ")
+        for curso, asignaturas in prof.asignaturas_a_impartir.items():
+            print(f"Grado: {curso}, Asignaturas: {asignaturas}")
+
 
     def buscar_en_departamentos(self, dni):
         miembro = self.__buscar_profesor(dni)
@@ -343,15 +360,13 @@ class Universidad:
         for tipo, miembros in self.miembros_dep.items():
             print(f'Departamento: {tipo}\nMiembros:')
             for member in miembros:
-                print(f"\tNombre: {member.nombre}")
-                print(f"\tDNI: {member.dni}")
-                print(f"\tDirección: {member.direccion}")
-                print(f"\tSexo: {member.sexo}\n")
+                print(member)
+                
 
     def __str__(self):
         txt = f"Universidad: {self.nombre}\n"
         
-        txt += r"** Profesores **\n"
+        txt += "** Profesores **\n"
         for profesor in self.profesores:
             txt += str(profesor) + "\n"
         
@@ -425,10 +440,10 @@ if __name__=='__main__':
     uni = Universidad("UPCT", "Cartagena", 30390)
 
     # Test insertar_estudiante
-    uni.insertar_estudiante("John Doe", "87654321C", "123 Main St", "V", "Data Science", "1st")
-    uni.insertar_estudiante("Carlos Garcia", "12345678A", "Calle Falsa 123", "V", "Mathematics", "1st")
-    uni.insertar_estudiante("Ana Martinez", "87654321B", "Avenida Siempre Viva 456", "M", "Physics", "2nd")
-    uni.insertar_estudiante("Pedro Lopez", "23456789C", "Paseo de la Castellana 789", "V", "Chemistry", "3rd")
+    uni.insertar_estudiante("John Doe", "87654321C", "123 Main St", "V", "Data Science", "1º")
+    uni.insertar_estudiante("Carlos Garcia", "12345678A", "Calle Falsa 123", "V", "Matemáticas", "1º")
+    uni.insertar_estudiante("Ana Martinez", "87654321B", "Avenida Siempre Viva 456", "M", "Física", "2º")
+    uni.insertar_estudiante("Pedro Lopez", "23456789C", "Paseo de la Castellana 789", "V", "Química", "3º")
 
     # Test insertar_prof_asociado
     uni.insertar_prof_asociado("Jane Smith", "23456789D", "456 Elm St", "M", TipoDepartamento(1))
@@ -436,28 +451,106 @@ if __name__=='__main__':
     uni.insertar_prof_asociado("Maria Gomez", "67890123H", "Avenida de la Luna 456", "M", TipoDepartamento(2))
 
     # Test insertar_prof_titular
-    uni.insertar_prof_titular("Alice Johnson", "34567890E", "789 Oak St", "OTRO", TipoDepartamento(2), "Computer Science")
-    uni.insertar_prof_titular("Jose Ramirez", "78901234I", "Paseo de las Estrellas 789", "V", TipoDepartamento(1), "Biology")
-    uni.insertar_prof_titular("Isabel Torres", "89012345J", "Calle de la Tierra 987", "M", TipoDepartamento(1), "Geology")
+    uni.insertar_prof_titular("Alice Johnson", "34567890E", "789 Oak St", "OTRO", TipoDepartamento(2), "Computación")
+    uni.insertar_prof_titular("Jose Ramirez", "78901234I", "Paseo de las Estrellas 789", "V", TipoDepartamento(1), "Biología")
+    uni.insertar_prof_titular("Isabel Torres", "89012345J", "Calle de la Tierra 987", "M", TipoDepartamento(1), "IA Generativa")
 
     # Test insertar_investigador
-    uni.insertar_investigador("Bob Williams", "45678901F", "987 Pine St", "V", TipoDepartamento(3), "Computer Science")
-    uni.insertar_investigador("Antonio Ruiz", "90123456K", "Avenida del Universo 321", "V", TipoDepartamento(1), "Astronomy")
-    uni.insertar_investigador("Carmen Morales", "01234567L", "Paseo de la Galaxia 654", "M", TipoDepartamento(2), "Astrophysics")
+    uni.insertar_investigador("Bob Williams", "45678901F", "987 Pine St", "V", TipoDepartamento(3), "Computación")
+    uni.insertar_investigador("Antonio Ruiz", "90123456K", "Avenida del Universo 321", "V", TipoDepartamento(1), "Astronomía")
+    uni.insertar_investigador("Carmen Morales", "01234567L", "Paseo de la Galaxia 654", "M", TipoDepartamento(2), "AstroFísica")
 
     # Insertar asignaturas
-    uni.insertar_asignatura("9990007", "Calculus", "Mathematics", 6,  PeriodoAsignatura(2))
-    uni.insertar_asignatura("9990035", "Physics", "Mathematics", 6, PeriodoAsignatura(1))
-    uni.insertar_asignatura("999006V", "Chemistry", "Chemistry", 6, PeriodoAsignatura(1))
-    uni.insertar_asignatura("99901AT", "Data Bases II", "Data Science", 6, PeriodoAsignatura(2))
-    uni.insertar_asignatura("99900XP", "Astronomy", "Physics", 6, PeriodoAsignatura(2))
-    uni.insertar_asignatura("999007I", "Astrophysics", "Physics", 6, PeriodoAsignatura(3))
+    uni.insertar_asignatura("9990123", "Machine Learning I", "Data Science", 6,  PeriodoAsignatura(2))
+    uni.insertar_asignatura("9990820", "Calculo", "Matemáticas", 6,  PeriodoAsignatura(2))
+    uni.insertar_asignatura("9990007", "Calculo", "Data Science", 6,  PeriodoAsignatura(2))
+    uni.insertar_asignatura("9990321", "Calculo", "Física", 6,  PeriodoAsignatura(2))
+    uni.insertar_asignatura("9990035", "Física", "Matemáticas", 6, PeriodoAsignatura(1))
+    uni.insertar_asignatura("999006I", "Química molecular II", "Química", 6, PeriodoAsignatura(1))
+    uni.insertar_asignatura("999006V", "Compuestos biorgánicos", "Química", 6, PeriodoAsignatura(1))
+    uni.insertar_asignatura("99901AT", "Bases Datos II", "Data Science", 6, PeriodoAsignatura(2))
+    uni.insertar_asignatura("99900XP", "Astronomía", "Física", 6, PeriodoAsignatura(2))
+    uni.insertar_asignatura("999007I", "AstroFísica", "Física", 6, PeriodoAsignatura(3))
     
-    print(uni)
-
-    uni.print_miembros_dep()
-
-    uni.cambio_departamento("89012345J", TipoDepartamento(3))
-    uni.cambio_departamento("90123456K", TipoDepartamento(2))
+    "COMENTARIO: BUENAS PROFESOR, DEJAMOS LOS COMANDOS COMENTADOS PARA QUE LE SEA MÁS FÁCIL LA VISUALIZACIÓN DE LO QUE REALIZA CADA MÉTODO."
     
-    uni.print_miembros_dep()
+    "IMPRESIÓN INFO UNIVERIDAD:"
+    #print(uni)
+    
+    "IMPRESIÓN DEPARTAMENTOS JUNTO A SUS MIEMBROS:"
+    #uni.print_miembros_dep()
+
+    "PODEMOS OBSERVAR COMO ANTONIO RUIZ Y ISABEL TORRES CAMBIAN DE DEPARTAMENTO(TAMBIÉN LO HACEN A NIVEL INTERNO)"
+    #uni.cambio_departamento("89012345J", TipoDepartamento(3))
+    #uni.cambio_departamento("90123456K", TipoDepartamento(2))
+
+    #uni.print_miembros_dep()
+
+    "ELIMINACIÓN DE OBJETOS DEL SISTEMA:"
+
+    '''for e in uni.estudiantes:
+        print(e)
+    uni.eliminar_estudiante("87654321C")
+
+    print("ALUMNO ELIMINADO DEL SISTEMA:")
+    for e in uni.estudiantes:
+        print(e)'''
+
+
+    '''uni.print_miembros_dep()
+    for p in uni.profesores:
+        print(p)
+    uni.eliminar_profesor("78901234I")  ERROR!!!!!!!!!
+
+    print("PROFESOR ELIMINADO DEL SISTEMA:")
+    for p in uni.profesores:
+        print(p)'''
+    
+
+    
+    '''for i in uni.investigadores:
+        print(i)
+    uni.eliminar_investigador("01234567L") 
+
+    print("INVESTIGADOR ELIMINADO DEL SISTEMA:")
+    for i in uni.investigadores:
+        print(i)
+    
+
+    uni.print_miembros_dep()'''
+
+
+    print()
+    print("MÉTODOS ASOCIADOS A ASIGNATURAS Y ESTUDIANTES:\n")
+
+    uni.añadir_asignatura_a_cursar("87654321C", "9990123")
+    uni.añadir_asignatura_a_cursar("87654321C", "99901AT")
+    
+    uni.aprobar_asignatura("87654321C", "9990123", 9)
+    uni.aprobar_asignatura("87654321C", "99901AT", 6)
+    
+    uni.visualizar_boletin_de_calificaciones("87654321C")
+    uni.visualizar_creditos_completados("87654321C")
+
+
+    print()
+    print("MÉTODOS ASOCIADOS A ASIGNATURAS Y PROFESORES:\n")
+    uni.asignar_profesorado_a_asignatura("89012345J", "9990123")
+    uni.asignar_profesorado_a_asignatura("56789012G", "9990820")
+    uni.visualizar_est_matriculados_y_profesores_asig("9990123") # ¿ Por qué no aparece en matriculados en Machine Learning I John Doe?
+    print("No hay alumnos matriculados \n")
+    uni.añadir_asignatura_a_cursar("12345678A", "9990123")              # Porque al aprobar la asignatura autómaticamente se le desmatricula.
+    uni.visualizar_est_matriculados_y_profesores_asig("9990123")
+    # Ahora si aparecería el alumno.
+
+    uni.eliminar_profesorado_de_asignatura("89012345J", "9990123") # Vemos como podemos quitar el puesto de profesor en una asignatura específica
+    uni.visualizar_est_matriculados_y_profesores_asig("9990123")
+
+
+    # Por último, veamos todas las asignaturas que imparte un profesor:
+    uni.asignar_profesorado_a_asignatura("34567890E", "9990820")
+    uni.asignar_profesorado_a_asignatura("34567890E", "9990035")
+    uni.asignar_profesorado_a_asignatura("34567890E", "9990007")
+    uni.asignar_profesorado_a_asignatura("34567890E", "9990321")
+
+    uni.visualizar_asignaturas_de_profesor("34567890E")
